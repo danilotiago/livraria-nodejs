93bcb8747e656ee58dbdfd6d1d8ee89756cd13c9
danilotiago <programacao.desenvolvimento@gmail.com>
2019-06-14 18:22:04 -0300

    encapsulando o express

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
+const app = require('./src/config/custom-express')
+
+app.get('/', function(req, resp) {
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Casa do código</h1>
+            </body>
+        </html>
+    `)
+})
+
+app.get('/livros', function(req, resp) {
+    resp.send(`
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Todos os livros</h1>
+            </body>
+        </html>
+    `)
+})
+
+app.listen(3000, function() {
+    console.log('Servidor iniciado')
+})
+
+/*
 const http = require('http')
 
 const server = http.createServer((req, resp) => {
@@ -14,4 +47,4 @@ const server = http.createServer((req, resp) => {
 })
 
 server.listen(3000)
-
+*/
