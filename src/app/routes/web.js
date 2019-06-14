4bd81a9a312a9f474edcaff0d809eb4ef6869575
danilotiago <programacao.desenvolvimento@gmail.com>
2019-06-14 19:55:15 -0300

    instalando marko

--- src/app/routes/web.js
+++ src/app/routes/web.js
@@ -13,15 +13,8 @@ module.exports = (app) => {
     })
     
     app.get('/livros', function(req, resp) {
-        resp.send(`
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1>Todos os livros</h1>
-                </body>
-            </html>
-        `)
+        resp.marko(
+            require('../views/books/list.marko')
+        )
     })
 }
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,6 +1,9 @@
 const app = require('express')()
 const routes = require('../app/routes/web')
 
+require('marko/node-require').install()
+require('marko/express')
+
 routes(app)
 
 module.exports = app
